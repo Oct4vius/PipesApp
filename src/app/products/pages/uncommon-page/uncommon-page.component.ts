b4b82eefa5e0332837fe51f8bd68a5a821cc1f2d
Octavio Luperón <armando2445@outlook.com>
2024-01-04 16:12:02 -0400

    Now i know all the current pipes

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrl: './uncommon-page.component.scss'
 })
 export class UncommonPageComponent {
+
+  // i18n Select
+  public name: string = 'Octavio';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  public changeClient(){
+    this.name = 'Melissa'
+    this.gender = 'female'
+  }
+
+
+  //i18Plural
+
+  public clients: string [] = ['Maria' , 'Pedro', 'Ivan', 'Melvin', 'Melissa', 'Robert', 'Octavio', 'Gabriel', 'Wilfred']
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos 1 cliente esperando.',
+    'other': 'tenemos # clientes esperando.',
+  }
+
+  public deleteClient(): void{
+    this.clients.shift()
+  }
+
+  // KeyValue Pipe
+  public person = {
+    name: 'Octavio',
+    age: '21',
+    address: 'Ottawa, Canada'
+  }
+
+  // Async Pipe
+  public myObservableTimer: Observable<number> = interval(2000)
+    .pipe(
+      tap( value => console.log('tap: ', value))
+    );
+
+  public promiseValue = new Promise<string>( (resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa.')
+      this.person.name = 'Otro nombre'
+    }, 3500)
+  })
 
 }
